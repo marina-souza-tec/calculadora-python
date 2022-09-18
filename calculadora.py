80aa0a02cdad945f6d5875888f44b7405dc45715
Marina <marina.alvessouzapl@gmail.com>
2022-09-18 12:29:19 -0300

    first commit

---
+++ calculadora.py
@@ -0,0 +1,3 @@
+first_number = int(input('digite o primeiro numero: '));\
+    second_number = int(input('digite o segundo numero: '));\
+        print("o resultado é igual a ", first_number + second_number)
